10213ce840504bbf8d308ea5e5cea9bab798ffa9
shortercode <iainshorter@gmail.com>
2020-04-25 11:47:23 +0100

    wip: update WeatherWidget to use data
    This displays the result either via a result component if data is successfully retrieved or via a message component if not.
    
    Additionally fixes the class !== className typos in the JSX

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,34 +1,78 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 // import logo from "./logo.png";
+import subscribeWeatherAtLocation from "../../WeatherSubscription";
 import './Weather.css';
+
+const APP_ID = "";
+const UPDATE_INTERVAL = 5000;
 
 export default function Weather () {
 	const [location, setLocation] = useState("London");
+	const [weatherData, setWeatherData] = useState(null);
+	const [error, setError] = useState("");
+
+	useEffect(() => {
+		const opts = {
+			appid: APP_ID, location, interval: UPDATE_INTERVAL
+		};
+		return subscribeWeatherAtLocation(opts, handleWeatherUpdate)
+	}, [location]);
 
 	function updateLocation (e) {
 		setLocation(e.target.value);
 	}
 
-	return <div class="weather-widget">
-		<h1 class="weather-widget__header">Your weather</h1>
-		<div class="weather-widget__body">
-			<label class="weather-widget__body__location">
+	function handleWeatherUpdate (result) {
+		if (result.isError()) {
+			setError(result.unwrapErr());
+			return;
+		}
+		setError("");
+		setWeatherData(result.unwrap());
+	}
+
+	let resultBlock;
+
+	if (error) {
+		resultBlock = <Message text={error}/>
+	}
+	else if (weatherData) {
+		resultBlock = <Result location={location} weatherData={weatherData}/>
+	}
+	else {
+		resultBlock = <Message text="loading"/>
+	}
+
+	return <div className="weather-widget">
+		<h1 className="weather-widget__header">Your weather</h1>
+		<div className="weather-widget__body">
+			<label className="weather-widget__body__location">
 				Enter your location:
 				<input 
-					class="weather-widget__body__location__input"
+					className="weather-widget__body__location__input"
 					type="text"
 					value={location}
-					onInput={updateLocation}/>
+					onChange={updateLocation}/>
 			</label>
-			<div class="weather-widget__body__result">
-				<h5 class="weather-widget__body__result__header">{location.toUpperCase()}</h5>
-				<div class="weather-widget__body__result__row">12.44 °C</div>
-				<div class="weather-widget__body__result__row">1032 hpa</div>
-				<div class="weather-widget__body__result__row">71 %</div>
-			</div>
-			<div class="weather-widget__body__update-timer">
+			{resultBlock}
+			<div className="weather-widget__body__update-timer">
 				Refreshing in 1m 5s
 			</div>
 		</div>
+	</div>
+}
+
+function Message ({ text }) {
+	return <div className="weather-widget__body__result">
+		{text}
+	</div>
+}
+
+function Result ({ weatherData, location }) {
+	return <div className="weather-widget__body__result">
+		<h5 className="weather-widget__body__result__header">{location.toUpperCase()}</h5>
+		<div className="weather-widget__body__result__row">{weatherData.temperature} °K</div>
+		<div className="weather-widget__body__result__row">{weatherData.pressure} hpa</div>
+		<div className="weather-widget__body__result__row">{weatherData.humidity} %</div>
 	</div>
 }
